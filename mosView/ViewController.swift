f5ea71b99249d0c15694357dac93b7b6ba458ca6
Tomohiko Himura <eiel.hal@gmail.com>
2015-11-18 19:53:37 +0900

    initial commit

---
+++ mosView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mosView
+//
+//  Created by eiel on 2015/11/18.
+//  Copyright © 2015年 eiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
