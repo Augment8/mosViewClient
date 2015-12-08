084831210874b5b3e21635a2c83e4fdcc8c6e2a0
Tomohiko Himura <eiel.hal@gmail.com>
2015-12-08 15:29:32 +0900

    参加者の名前を表示

--- mosView/ViewController.swift
+++ mosView/ViewController.swift
@@ -13,14 +13,10 @@ import SpriteKit
 import SpriteKit
 
 class GameScene: SKScene {
+    var users = [NSString: NSDictionary]()
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -46,10 +42,25 @@ class GameScene: SKScene {
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    typealias User = NSDictionary
+    func updateUser(id: NSString, user: User) {
+        if let _ = users[id] {
+        } else {
+            let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+            if let name = user["name"] {
+                myLabel.text = name as? String
+            }
+            myLabel.fontSize = 45;
+            myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+            
+            self.addChild(myLabel)
+        }
+        users[id] = user
+    }
 }
 
 class ViewController: UIViewController, WebSocketDelegate {
-
+    
     func websocketDidConnect(socket: WebSocket) {
         NSLog("websocketDidConnect")
     }
@@ -61,6 +72,7 @@ class ViewController: UIViewController, WebSocketDelegate {
         do {
             let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! NSDictionary
             NSLog("%@", json)
+            _scene.updateUser(json["id"] as! NSString, user: json)
         } catch {
             NSLog("json serializa error")
         }
@@ -71,13 +83,14 @@ class ViewController: UIViewController, WebSocketDelegate {
     
 
     let socket: WebSocket = WebSocket(url: NSURL(string: "ws://au8mos.herokuapp.com")!, protocols: ["mos-view"])
+    var _scene: GameScene! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         socket.delegate = self
         socket.connect()
-        
         if let scene = GameScene(fileNamed:"GameScene") {
+            _scene = scene
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
@@ -87,7 +100,7 @@ class ViewController: UIViewController, WebSocketDelegate {
             skView.ignoresSiblingOrder = true
             
             /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+            scene.scaleMode = .AspectFit
             
             skView.presentScene(scene)
         }
